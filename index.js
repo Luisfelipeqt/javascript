0104156ba0e9d82432d48cd9dade5ef84dd70b38
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-15 20:30:02 -0300

    Adicionando if e else do resultado no html

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 
 
 let value = Number(prompt("Enter the product value!"))
-console.log(typeof(value))
+
+if(value >= 20){
+    document.getElementById("result").innerHTML = "Approved!"
+}
+else {
+    document.getElementById("result").innerHTML = "Denied!"
+
+}
